0ff23ffc39189462b6764968f488a1ce83a673ce
Indra <indraberga@inbox.lv>
2021-10-13 19:36:55 +0300

    Lesson7_begin

---
+++ Lesson7_Demo/Lesson7_Demo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lesson7_Demo
+{
+    class Program
+    {
+        private static int sumOfMin;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            sumOfMin = 14;
+        }
+
+        public void Sum()           //metode nav static, tāpēc nevar izmantot Main metodē
+        {
+            Console.WriteLine("Can I sum here?");
+
+            StaticSum();
+        }
+
+        public static void StaticSum()
+        {
+            Console.WriteLine("Can I sum here?");
+        }
+    }
+}
