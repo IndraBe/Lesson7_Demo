38a6b2291b66784bda5d2d080698b4fca5b2bf06
Indra <indraberga@inbox.lv>
2021-10-13 20:21:14 +0300

    Lesson7_3rd part

--- Lesson7_Demo/Lesson7_Demo/Program.cs
+++ Lesson7_Demo/Lesson7_Demo/Program.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 
+//https://docs.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-5.0
+
 namespace Lesson7_Demo
 {
     class Program
     {
-        private static int sumOfMin;
+        private static int sumOfMin;        //beigs eksistēt, kad izslēgs programmu. Viens uz visu programmu. Automātiski programmas beigās iznīcināts.
+
+
 
         static void Main(string[] args)
         {
@@ -18,10 +22,23 @@ namespace Lesson7_Demo
             currentDate.GetYesterday();
 
 
-
             Helper.Divide();
             Helper.age = 14;
-        }
+
+            //Garbage Collector
+
+            int sum = 48;
+           
+            if (true)
+            {
+                sum++;
+            }
+
+            else
+            {
+                sum--;
+            }
+        }               //beidz savu darbību pēc šī un var tikt uznīcināts
 
         public void Sum()           //metode nav static, tāpēc nevar izmantot Main metodē
         {
